df76de026ea25614e18c856dfb4286e67aceb30d
Case Brophy <cbrophy@superiortech.io>
2026-03-27 11:32:58 -0600

    test(sdk): add unitest table-driven test runner

---
+++ business/sdk/unitest/model.go
@@ -0,0 +1,12 @@
+// Package unitest provides support for executing unit test logic.
+package unitest
+
+import "context"
+
+// Table represent fields needed for running a unit test.
+type Table struct {
+	Name    string
+	ExpResp any
+	ExcFunc func(ctx context.Context) any
+	CmpFunc func(got any, exp any) string
+}
